2c8574411c837d3c517594aae26c75e3e6eb7bad
YDK <94510368+IDENTUNECHI@users.noreply.github.com>
2024-10-24 22:17:01 +0900

    [Bronze II] Title: 수열의 비밀 (Easy), Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/32279. 수열의 비밀 （Easy）/수열의 비밀 （Easy）.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // 배열 입력 받기
+        int n = input.nextInt();
+        int p = input.nextInt();
+        int q = input.nextInt();
+        int r = input.nextInt();
+        int s = input.nextInt();
+        int a_1 = input.nextInt();
+
+        int sum = 0;
+        int [] list = new int [n*20];
+        list[1] = a_1;
+
+        for(int i=1; i<n; i++) {
+            list[2*i] = p*list[i]+q;
+            list[2*i+1] = r*list[i]+s;
+        }
+
+        for(int i=1; i<=n; i++){
+            sum += list[i];
+        }
+         // n이 짝수 일때
+
+        System.out.println(sum);
+    }
+}
