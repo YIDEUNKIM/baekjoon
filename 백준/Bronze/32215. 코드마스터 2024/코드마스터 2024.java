1436702996f8688132efdfdde0d80043a579ebfb
YDK <94510368+IDENTUNECHI@users.noreply.github.com>
2024-11-07 19:04:13 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int k = input.nextInt();
+        
+        System.out.println(m*k + m);
+        
+    }
+
+}
