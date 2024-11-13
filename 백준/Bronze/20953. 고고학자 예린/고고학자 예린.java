df38bf7a60b6fbf12f7a33165619746f2aacba17
YDK <94510368+IDENTUNECHI@users.noreply.github.com>
2024-11-13 21:36:50 +0900

    [Bronze II] Title: 고고학자 예린, Time: 1680 ms, Memory: 187744 KB -BaekjoonHub

--- 백준/Bronze/20953. 고고학자 예린/고고학자 예린.java
+++ 백준/Bronze/20953. 고고학자 예린/고고학자 예린.java
@@ -1,27 +1,22 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static long dolmen(int a, int b) {
         long sum = 0;
-        int result = (int) Math.pow((a+b), 2);
-        sum = sum + (long)result * (a + b - 1) / 2;
+        
+        sum = (long) (a+b)*((a+b) * (a + b - 1) / 2);
         return sum;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        ArrayList<Long> arr = new ArrayList<>();
         for(int i = 0; i < n; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            arr.add(dolmen(a,b));
+            System.out.println(dolmen(a,b));
         }
-        for(long x : arr) {
-            System.out.println(x);
-        }
+        
 
     }
 
