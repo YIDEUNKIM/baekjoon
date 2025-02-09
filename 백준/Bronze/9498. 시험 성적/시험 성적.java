d00bb3fcdf26f241a8beffb83cdbefdc3744d161
YIDEUN_KIM <94510368+IDENTUNECHI@users.noreply.github.com>
2025-02-09 12:31:22 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,20 +1,26 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
 
-        if(89 < a )
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(90 <= n && n <= 100) {
             System.out.println("A");
-        else if (79<a)
+        }
+        else if(80 <= n && n < 90) {
             System.out.println("B");
-        else if (69<a)
-                System.out.println("C");
-        else if (59 < a)
-                    System.out.println("D");
-
-
-        else
+        }
+        else if(70 <= n && n < 80) {
+            System.out.println("C");
+        }
+        else if(60 <= n && n < 70) {
+            System.out.println("D");
+        }
+        else 
             System.out.println("F");
 
 
