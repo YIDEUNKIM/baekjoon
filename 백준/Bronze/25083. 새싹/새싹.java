f7a7e340a1366eb6986fc9a025486c74c785fcaa
YIDEUN_KIM <94510368+IDENTUNECHI@users.noreply.github.com>
2025-02-09 12:56:42 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17488 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+            System.out.println("         ,r'\"7\n" +
+                    "r`-_   ,'  ,/\n" +
+                    " \\. \". L_r'\n" +
+                    "   `~\\/\n" +
+                    "      |\n" +
+                    "      |");
+
+
+    }
+}
