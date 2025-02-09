4f67fa7b4c2ebe19ce2f067f2c75179af70c58b1
YIDEUN_KIM <94510368+IDENTUNECHI@users.noreply.github.com>
2025-02-09 12:34:42 +0900

    [Bronze V] Title: 고양이, Time: 160 ms, Memory: 17296 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,18 @@
-public class Main{
-    public static void main(String[] args){
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
         System.out.println(" \\(__)|");
+
+
+
     }
 }
