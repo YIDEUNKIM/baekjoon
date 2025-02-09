574f274f24629af4de219f0f819851058152b989
YIDEUN_KIM <94510368+IDENTUNECHI@users.noreply.github.com>
2025-02-09 12:54:21 +0900

    [Bronze V] Title: A×B, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,10 +1,17 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            System.out.println(b * c);
         
-        int a = input.nextInt();
-        int b = input.nextInt();
-        System.out.println(a*b);
+
     }
 }
