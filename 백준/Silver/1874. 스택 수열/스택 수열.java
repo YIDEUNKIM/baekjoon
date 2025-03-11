f6aba60309538491e3909300639ab08744aa439f
YIDEUN_KIM <94510368+IDENTUNECHI@users.noreply.github.com>
2025-03-11 10:25:50 +0900

    [Silver II] Title: 스택 수열, Time: 1852 ms, Memory: 132872 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -8,37 +8,37 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
         List<Character> printer = new ArrayList<>();
-
         int n = input.nextInt();
-        int[] sequence = new int[n]; // 입력 수열
-        for (int i = 0; i < n; i++) {
-            sequence[i] = input.nextInt();
+        
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            list.add(input.nextInt());
         }
 
-        int num = 1; // 스택에 넣을 다음 숫자
-        int idx = 0; // 수열의 현재 위치
+        int i = 1;
+        int j = 0;
 
-        while (idx < n) {
-            if (!stack.isEmpty() && stack.peek() == sequence[idx]) {
-                // 스택 맨 위가 현재 수열 값과 같으면 pop
+        while(j < n) {
+            if(!stack.isEmpty() && stack.peek().equals(list.get(j))) {
                 stack.pop();
                 printer.add('-');
-                idx++;
-            } else if (num <= n) {
-                // 다음 숫자를 push
-                stack.push(num);
+                j++;
+            }
+            else if(i <= n){
+                stack.push(i);
                 printer.add('+');
-                num++;
-            } else {
-                // 더 이상 push할 숫자가 없고 pop도 불가능하면 실패
+                i++;
+            }
+            else {
                 System.out.println("NO");
                 return;
             }
         }
 
-        // 수열을 모두 만들었다면 연산 출력
-        for (char op : printer) {
-            System.out.println(op);
+
+        for(char a : printer) {
+            System.out.println(a);
         }
+
     }
 }
