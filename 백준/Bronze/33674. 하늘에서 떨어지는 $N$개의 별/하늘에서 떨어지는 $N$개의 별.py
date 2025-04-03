d1537b7e19f0e39344b6a258d3fcf5dfb1fbf76a
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-04-03 11:02:13 +0900

    Update 하늘에서 떨어지는 $N$개의 별.py

--- 백준/Bronze/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.py
+++ 백준/Bronze/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.py
@@ -2,20 +2,20 @@ n, d, k = map(int, input().split())
 # n개의 점, d일동안, 별이 k개 초과
 s = list(map(int, input().split()))
 
-value = max(s)
-count = 0
-f = [0 for _ in range(n)]
+value = max(s) # s의 최댓값 할당
+count = 0 # 결과값 카운팅
+f = [0 for _ in range(n)] f 초기화
 
 
 
-for i in range(d):
+for i in range(d): # d일 동안 순회
 
-    if max(f)+ value > k:
+    if max(f)+ value > k: # s의 최댓값 + 그 다음날 더해질 별의 갯수 > k 일때 count = count + 1
         f = [0 for _ in range(n)]
         count += 1
     
     for j in range(n):
-        f[j] += s[j]
+        f[j] += s[j] # 다음날로 넘어갈 때 별 쌓임
 
 
-print(count)
+print(count) # 결과 출력
