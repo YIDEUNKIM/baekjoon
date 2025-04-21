c688f6177e9207c347417e52c7a523a42f9bc868
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-04-21 19:44:55 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.c
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.c
@@ -1,81 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct Node {
-    int data;
     int number;
+    int value;
     struct Node * next;
     struct Node * prev;
 } Node;
 
-int main(void) {
+int main() {
     int n;
-
     scanf("%d", &n);
 
     Node * head = NULL;
     Node * tail = NULL;
+
     for (int i=0; i<n; i++) {
-        int element;
-        scanf("%d", &element);
+        int number;
+        scanf("%d", &number);
 
-        Node * new_node = (Node *)malloc(sizeof(Node));
-        new_node -> data = i+1;
-        new_node -> number = element;
+        Node * new_node = (Node*)malloc(sizeof(Node));
 
+        new_node -> number = i+1;
+        new_node -> value = number;
         if (head == NULL) {
             new_node -> next = new_node;
             new_node -> prev = new_node;
             head = new_node;
             tail = new_node;
-        }
-        else {
-            new_node -> prev = tail;
+        } else {
             new_node -> next = head;
-
-            head->prev = new_node;
+            new_node -> prev = tail;
+            head -> prev = new_node;
             tail -> next = new_node;
             tail = new_node;
         }
     }
 
-    //pop ballons
-    struct Node * current = head;
+    Node * current = head;
+    // pop ballon
     for (int i=0; i<n; i++) {
-        // print current balloon number
-        printf("%d ", current->data);
+        // print
+        printf("%d ", current -> number);
         if (i == n-1)
             break;
-        // store move_steps
-        int move_steps = current -> number;
-        struct Node *next_node = current->next;
-        struct Node *prev_node = current->prev;
 
-        //Remove current node from the list
+        Node * next_node = current->next;
+        Node * prev_node = current -> prev;
+        // store next move_step & remove node from list
+        int move_step = current -> value;
         next_node -> prev = prev_node;
         prev_node -> next = next_node;
 
-
-        // Update head if removing the head
-        if (current == head) {
-            head = next_node;
-        }
-
-        Node * deletion = current;
-        if (move_steps > 0) {
-            for (int i=0; i<move_steps; i++) {
+        // search node
+        Node * dele = current;
+        if (move_step > 0) {
+            for (int j=0; j<move_step; j++) {
                 current = current -> next;
             }
         } else {
-            move_steps = -1 * move_steps;
-            for (int i=0; i<move_steps; i++) {
+            for (int j=0; j<-move_step; j++) {
                 current = current -> prev;
             }
         }
-        // delete node
-        free(deletion);
-    }
-    if (head != NULL) {
-        free(head);
+        free(dele);
     }
 
     return 0;
