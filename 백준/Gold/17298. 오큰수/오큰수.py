0ef014a488bad11ebdb3b392e3e8e98127069833
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-09-28 16:51:09 +0900

    [Gold IV] Title: 오큰수, Time: 448 ms, Memory: 251940 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,21 @@
+# 오큰수
+n = int(input())
+
+a = list(map(int, input().split()))
+
+stack = []
+stack.append(0)
+ans = [0] * n
+
+for i in range(n):
+    while(stack and a[i] > a[stack[-1]]):
+        ans[stack.pop()] = a[i]
+    stack.append(i)
+
+while(stack):
+    ans[stack.pop()] = -1
+
+for i in range(n):
+    print(str(ans[i]), end=" ")
+
+# 
