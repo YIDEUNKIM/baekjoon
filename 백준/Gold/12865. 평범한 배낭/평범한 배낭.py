dcdbb167fe89352455bf37c06e895b7e661da1af
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-12-01 13:03:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 332 ms, Memory: 188728 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -14,9 +14,9 @@ dp = [[0 for _ in range((k+1))] for _ in range(n+1)]
 for i in range(1, n+1):
     for w in range(1, k+1):
         if weight[i-1] <= w:
-            dp[i][w] = max(dp[i-1][w],
+            dp[i][w] = max(dp[i-1][w], 
                            dp[i-1][w-weight[i-1]] + value[i-1])
         else:
             dp[i][w] = dp[i-1][w]
-
-print(max(map(max, dp)))
+            
+print(dp[n][k])
