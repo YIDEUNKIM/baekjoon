0f7fa5d43cf267e3a9f846a1fcaa6f65ae4bdb45
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-12-02 19:40:23 +0900

    [Gold IV] Title: 최단경로, Time: 444 ms, Memory: 138864 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,43 +1,37 @@
+# 최단 경로
 import heapq
 import sys
 
 INF = sys.maxsize
+
 v, e = map(int, input().split())
 k = int(input())
-dp = [INF] * (v+1)
+
 graph = [[] for _ in range(v+1)]
-heap = []
+mdistance = [INF for _ in range(v+1)]
+q = []
+heapq.heappush(q, (0, k))
+visited = [False]* (v+1)
+mdistance[k] = 0
 
-def dijkstra(start):
-    # 가중치 테이블에서
-    # 시작 정점에 해당하는 가중치는 0으로 초기화
-    dp[start] = 0
-    heapq.heappush(heap, (0, start))
-
-    #힙에 원소가 없을 때 까지 반복.
-    while heap:
-        # 가중치, 현재 노드
-        wei, now = heapq.heappop(heap)
-
-        # if unnecessary tuple is ignored comparing current table
-        if dp[now] < wei:
-            continue
-
-        for w, next_node in graph[now]:
-            #현재 정점까지의 가중치 wei + 현재 정점에서 다음 정점까지의 가중치 W
-            # = to next node weight(next_wei)
-            next_wei = w + wei
-            # 다음 노드까지의 가중치가 현재 기록된 값 보다 작으면 조건 성립
-            if next_wei < dp[next_node]:
-                # 계산했던 next_wei를 가중치 테이블에 업뎃
-                dp[next_node] = next_wei
-                #다음 점 까지의 가중치와 다음 점에 대한 정보를 튜플로 묶어 최소 힙에 삽입
-                heapq.heappush(heap, (next_wei, next_node))
-
-for _ in range(e):
+for i in range(e):
     x, y, z = map(int, input().split())
-    graph[x].append((z,y))
+    graph[x].append((y,z))
 
-dijkstra(k)
-for i in range(1, v+1):
-    print("INF" if dp[i] == INF else dp[i])
+while q:
+    w, v = heapq.heappop(q)
+
+    if not visited[v]:
+        visited[v] = True
+        for neighbor, weight in graph[v]:
+            next_wei = w + weight
+            if next_wei < mdistance[neighbor]:
+                mdistance[neighbor] = next_wei
+                heapq.heappush(q, (next_wei, neighbor))
+
+
+for i in range(1, len(mdistance)):
+    if mdistance[i]==INF:
+        print("INF")
+    else:
+        print(mdistance[i])
