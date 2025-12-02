458f9b0567e68af06adce7e81258e008f74d49e8
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-12-02 14:16:40 +0900

    [Gold V] Title: CCW, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,17 @@
+# CCW
+
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+result = (x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3)
+
+ans = 0
+if result > 0:
+    ans = 1
+elif result < 0:
+    ans = -1
+else:
+    ans = 0
+    
+print(ans)
