5c247a2f37e2e02376df879f2629c861a96aa398
YIDEUN_KIM <94510368+YIDEUNKIM@users.noreply.github.com>
2025-12-30 19:47:34 +0900

    [Bronze IV] Title: 셔틀 탈래 말래 탈래 말래 애매하긴 해, Time: 104 ms, Memory: 110064 KB -BaekjoonHub

---
+++ 백준/Bronze/34946. 셔틀 탈래 말래 탈래 말래 애매하긴 해/셔틀 탈래 말래 탈래 말래 애매하긴 해.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+a, b, c, d = map(int, input().split())
+
+
+if (a+b <= d) and (c<=d):
+    print("~.~")
+elif (a+b <= d):
+    print("Shuttle")
+elif (c<=d):
+    print("Walk")
+else:
+    print("T.T")
